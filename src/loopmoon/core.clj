01717c487c15cfe31ec86d294df46296465b50b4
Kerby Ferris <kerbyferris@gmail.com>
2016-11-19 15:52:59 -0800

    clean up core

--- src/loopmoon/core.clj
+++ src/loopmoon/core.clj
@@ -1,8 +1,1 @@
-(ns loopmoon.core
-  (:use overtone.live))
-
-(stop)
-
-(demo 10
-      (lpf (mix (saw [400 (line 100 1600 5) 101 100.5]))
-           (lin-lin (lf-tri (line 2 20 5)) -1 1 400 4000)))
+(ns loopmoon.core)
